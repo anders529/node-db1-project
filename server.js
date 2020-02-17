e1d8ef675f3459ae09d03132a52a4c897e019758
Aaron Andersen <anderseana@tutanota.com>
2020-02-17 15:15:27 -0600

    MVP is met. Files have been cleaned up.

--- server.js
+++ server.js
@@ -1,31 +1,6 @@
 const express = require('express');
-const db = require('./data/dbConfig.js');
+const accountRoutes = require('./routes/accountRoutes');
 const server = express();
 server.use(express.json());
-server.get("/", (req, res) => {
-    db.select('*')
-        .from('accounts')
-        .then(accounts => res.status(200).send(accounts))
-        .catch(err => res.status(500).send({message: 'Accounts were not able to be found.', error: err}));
-});
-server.post('/', (req, res) => {
-    db('accounts')
-        .insert(req.body)
-        .then(account => res.status(201).send({response: account, message: 'Account successfully added.'}))
-        .catch(err => res.status(500).send({error: err, message: 'Account was not added to the database'}));
-});
-server.put('/:id', (req, res) => {
-    db('accounts')
-        .where({id: req.params.id})
-        .update(req.body)
-        .then(account => res.status(200).send({result:account, message:'Account successfully updated'}))
-        .catch(err => {res.status(500).send({errorMessage:'Account was not updated', error: err})});
-});
-server.delete('/:id', (req, res) => {
-    db('accounts')
-        .where({id: req.params.id})
-        .del()
-        .then(response => res.status(200).send({message:'Account successfully deleted', response: response}))
-        .catch(err => res.status(500).send({errorMessage:'Account was not deleted', error: err}));
-});
+server.use('/', accountRoutes);
 module.exports = server;
