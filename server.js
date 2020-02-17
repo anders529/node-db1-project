930e30442ee4377c458392388cc768a822d50590
Aaron Andersen <anderseana@tutanota.com>
2020-02-17 15:12:03 -0600

    Forgot to commit the files earlier!! Sorry about that!! MVP is met.

--- server.js
+++ server.js
@@ -3,31 +3,29 @@ const db = require('./data/dbConfig.js');
 const server = express();
 server.use(express.json());
 server.get("/", (req, res) => {
-    db.select("*")
-        .from("accounts")
+    db.select('*')
+        .from('accounts')
         .then(accounts => res.status(200).send(accounts))
-        .catch(err => res.status(500).send({message: "Accounts were not able to be found.", error: err}));
+        .catch(err => res.status(500).send({message: 'Accounts were not able to be found.', error: err}));
 });
-server.post("/", (req, res) => {
-    db("accounts")
+server.post('/', (req, res) => {
+    db('accounts')
         .insert(req.body)
-        .then(account =>
-            res.status(201).send({response: account, message: "Account successfully added." })
-        )
-        .catch(err => res.status(500).send({error: err, message: "Account was not added to the database" }));
+        .then(account => res.status(201).send({response: account, message: 'Account successfully added.'}))
+        .catch(err => res.status(500).send({error: err, message: 'Account was not added to the database'}));
 });
-server.put("/:id", (req, res) => {
-    db("accounts")
+server.put('/:id', (req, res) => {
+    db('accounts')
         .where({id: req.params.id})
         .update(req.body)
-        .then(account => res.status(200).send({result:account, message:"Account successfully updated"}))
-        .catch(err => {res.status(500).send({errorMessage:"Account was not updated", error: err})});
+        .then(account => res.status(200).send({result:account, message:'Account successfully updated'}))
+        .catch(err => {res.status(500).send({errorMessage:'Account was not updated', error: err})});
 });
-server.delete("/:id", (req, res) => {
-    db("accounts")
+server.delete('/:id', (req, res) => {
+    db('accounts')
         .where({id: req.params.id})
         .del()
-        .then(response => res.status(200).send({message:"Account successfully deleted", response: response}))
-        .catch(err => res.status(500).send({errorMessage:"Account was not deleted", error: err}));
+        .then(response => res.status(200).send({message:'Account successfully deleted', response: response}))
+        .catch(err => res.status(500).send({errorMessage:'Account was not deleted', error: err}));
 });
 module.exports = server;
